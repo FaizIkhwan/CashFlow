80a8f9ded87ad3411c628fd9efab762ca98067c8
Faiz Kwan <faizikhwantop@gmail.com>
2018-05-29 01:15:48 +0800

    expense

--- app/src/main/java/com/fish/cashflow/Expense.java
+++ app/src/main/java/com/fish/cashflow/Expense.java
@@ -9,5 +9,6 @@ public class Expense extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expense);
+        //to be continue
     }
 }
